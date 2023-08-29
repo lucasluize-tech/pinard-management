817c11c5d3be2d3a5acf02165308d52a0ac3a365
Lucas Luize <lucasluize.tech@gmail.com>
2023-08-29 10:22:39 -0400

    prisma first migration with nextauth models and nextauth route with google provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
 import db from "@/db/db";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
